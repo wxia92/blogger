f7f672119628cbefecdcef5aef35f6b220a0bec7
William Xia <wxia1992@gmail.com>
2018-11-26 12:16:12 -0500

    finish blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,13 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
+		@comment = Comment.new
+		@comment.article_id = @article.id
 	end
 
 	def new
 		@article = Article.new
+
 	end
 
 	def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Blogger::Application.routes.draw do
   root to: 'articles#index'
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+  
 end
 
